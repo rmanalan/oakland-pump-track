b5f495748a7144872180713150c650c5ff434a14
Rich Manalang <rmanalang@launchdarkly.com>
2020-10-10 23:33:55 -0700

    Increase font size on mobile

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({children}) {
   return (
     <>
       <Hero />
-      <div className="main prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto px-5">
+      <div className="main prose lg:prose-lg xl:prose-2xl mx-auto px-5">
         {children}
       </div>
     </>
