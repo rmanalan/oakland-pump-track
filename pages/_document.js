8e0de0925a356f5c553a5d179f5cda40b0e481c1
Rich Manalang <rmanalang@launchdarkly.com>
2020-10-11 14:39:41 -0700

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="description" content="Situated in the Joaquin Miller Park, the Oakland Pump Track is now open to the public!"/>
+          <link rel="icon" type="image/svg+xml" href={require("../public/images/logo.svg")}></link>
           <link href={`https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|EB+Garamond&display=swap`} rel="stylesheet"></link>
         </Head>
         <body>
